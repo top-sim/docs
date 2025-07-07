74e0f4a5fab26ff77a45c176b14c4db6f060ca21
myxie <contact@ryanbunney.com>
2025-07-07 20:16:23 +0800

    Update topsim documentation

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('topsim_pipelines/'))
 # -- Project information -----------------------------------------------------
 
 project = 'TopSim'
-copyright = '2021, RW Bunney'
+copyright = '2025, RW Bunney'
 author = 'RW Bunney'
 
 
@@ -50,7 +50,7 @@ exclude_patterns = ['venvdocs/*.rst']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = ''
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
